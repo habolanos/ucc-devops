2ea5d218c46ea000bfacdad92182a293442ce847
Harold Adrian Bolaños Rodriguez <harold.bolanosrod@campusucc.edu.co>
2021-09-02 20:02:19 -0500

    01. mi primer commit en git para el programa

---
+++ my_program.cpp
@@ -0,0 +1,23 @@
+<stdio.h>
+<math.h>
+<getch.h>
+
+#workspace 'curso devops'
+
+static int main[object ...] {
+
+	int ivalor = 0;
+	int icontador= 0;
+
+	cin << 'Ingrese un valor para calcular una suma';
+
+	for (i = 1; i <= ivalor; i++) {
+		icontador+=i;
+	}
+
+	cout >> 'El resultado es: '+%int,icontador; 
+
+	getch();
+
+	exit 1;
+}
