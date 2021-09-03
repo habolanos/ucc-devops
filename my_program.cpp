e098abbf97ab61775738326064794e9855c286b8
Harold Adrian Bolaños Rodriguez <harold.bolanosrod@campusucc.edu.co>
2021-09-02 21:09:19 -0500

    04. se add .gitignore con contenido, actualizamos el fuente de my_program.cpp

--- my_program.cpp
+++ my_program.cpp
@@ -8,16 +8,17 @@ int main() {
 
 	int ivalor = 0;
 	int icontador= 0;
+	int i = 0;
 
-	cin >> "Ingrese un valor para calcular una suma";
+	cout << "Ingrese un valor para calcular una suma.\n";
+	cin >> ivalor;
+	cout << "Usted ingreso: " << ivalor << endl;
 
 	for (i = 1; i <= ivalor; i++) {
 		icontador+=i;
 	}
 
-	cout << "El resultado es: " << icontador; 
-
-	getch();
+	cout << "El resultado es: " << icontador << endl; 
 
 	return 0;
 }
